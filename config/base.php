7e88480303fe0a1f8b9c49da7e5c6f87bdbf5292
jupiter9381 <jupiter9381@gmail.com>
2020-05-29 08:01:19 +1000

    signin

---
+++ config/base.php
@@ -0,0 +1,4 @@
+<?php 
+	define("SITE_URL", "http://dev.backoffice.com/");
+	define("ASSET_URL", SITE_URL . 'assets/');
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+	session_start();
+	include("config/base.php");
+
+	header('Location: '.SITE_URL.'signin.php');
+?>
